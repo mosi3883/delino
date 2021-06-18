ae0e6cdad76cbd63e669b89fb52f0c885f410f8b
Mostafa Bodagh Abadi <54463682+mosi3883@users.noreply.github.com>
2021-06-19 00:19:14 +0430

    mobile nav

--- js/script.js
+++ js/script.js
@@ -2,18 +2,21 @@ const ham = document.querySelector(".hamberger");
 const hamIcon = document.querySelector(".ham__icon.icon-menu");
 const closeIcon = document.querySelector(".ham__icon.icon-close");
 const overlay = document.querySelector(".overlay")
-ham.addEventListener("click", function () {
+ham.addEventListener("click", function (e) {
+  e.preventDefault();
   if (ham.dataset.state === "0") {
     //alert("its close, openning");
     hamIcon.classList.add("hidden")
     closeIcon.classList.remove("hidden");
     ham.dataset.state = "1";
     overlay.classList.remove("hidden");
+    setTimeout(() => overlay.style.opacity = "1", 0)
   } else {
     //alert("its open, closing");
     hamIcon.classList.remove("hidden")
     closeIcon.classList.add("hidden");
     ham.dataset.state = "0";
     overlay.classList.add("hidden");
+    setTimeout(() => overlay.style.opacity = "0", 0)
   }
 })
